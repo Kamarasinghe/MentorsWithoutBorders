391dddc1b49690b692d9feb1cfbc402f0f9f9133
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-06 15:00:13 -0700

    Created a capitalize words function/ran names and states through it

--- client/components/MentorSearchComponents/SelectedMentors.jsx
+++ client/components/MentorSearchComponents/SelectedMentors.jsx
@@ -3,6 +3,18 @@ import ListItem from '@material-ui/core/ListItem';
 import Avatar from '@material-ui/core/Avatar';
 
 export default class SelectedMentors extends Component {
+
+  capitalize (words) {
+    words = words.split(' ');
+    let capitalized = [];
+
+    words.forEach((word) => {
+      capitalized.push(`${word[0].toUpperCase()}${word.slice(1)}`);
+    })
+
+    return capitalized.join(' ');
+  }
+
   render() {
     let mentor = this.props.mentor;
 
@@ -11,13 +23,13 @@ export default class SelectedMentors extends Component {
         <ListItem>
           <Avatar src={mentor.picture.thumbnail} className='theAvatar' />
             <div className='theName'>
-              <h2>{`${mentor.name.first} ${mentor.name.last}`}</h2> 
+              <h2>{`${this.capitalize(`${mentor.name.first} ${mentor.name.last}`)}`}</h2> 
             </div>
             <div className='state'>
-              <h3>{mentor.location.state}</h3> 
+              <h3>{`${this.capitalize(mentor.location.state)}`}</h3> 
             </div>
         </ListItem>
-        {console.log(this.props.mentor)}
+        {/* {console.log(this.props.mentor)} */}
       </div>
     );
   }
