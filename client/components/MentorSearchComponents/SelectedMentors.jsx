a267ba059bd4dd1ccd926f1b47c0baccbac2a9ef
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-06 19:31:23 -0700

    Incorporated font-awesome comments, user-plus and id-card icons

--- client/components/MentorSearchComponents/SelectedMentors.jsx
+++ client/components/MentorSearchComponents/SelectedMentors.jsx
@@ -22,12 +22,24 @@ export default class SelectedMentors extends Component {
       <div className='eachMentor'>
         <ListItem>
           <Avatar src={mentor.picture.thumbnail} className='theAvatar' />
-            <div className='theName'>
-              <h2>{`${this.capitalize(`${mentor.name.first} ${mentor.name.last}`)}`}</h2> 
-            </div>
-            <div className='state'>
-              <h3>{`${this.capitalize(mentor.location.state)}`}</h3> 
-            </div>
+          <div className='theName'>
+            <h2>{`${this.capitalize(`${mentor.name.first} ${mentor.name.last}`)}`}</h2> 
+          </div>
+          <div className='state'>
+            <h3>{`${this.capitalize(mentor.location.state)}`}</h3> 
+          </div>
+          <div className='aQuote'>
+            <h2>A pretty cool quote from the mentor can go here...</h2>
+          </div>
+          <div className='startChat'>
+            <i className='far fa-comments fa-2x chatBubble' />
+          </div>
+          <div className='addMentorFA'>
+            <i className='fas fa-user-plus fa-2x addMentor'></i>
+          </div>
+          <div className='profileFA'>
+            <i className='far fa-id-card fa-2x viewProfile'></i>
+          </div>
         </ListItem>
         {/* {console.log(this.props.mentor)} */}
       </div>
